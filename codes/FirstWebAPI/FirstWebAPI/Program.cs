dc43577d48cf1753c43de61ea24cd62583c67160
niltor <zpty@outlook.com>
2023-12-20 17:47:11 +0800

    更新教程文档

---
+++ codes/FirstWebAPI/FirstWebAPI/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// 默认路由返回
+app.Map("/", () => "Hello World!");
+// 返回请求内容
+app.Map("/{name}", (string name) => "Hello " + name);
+
+// 使用控制器默认路由模板
+//app.MapDefaultControllerRoute();
+
+app.MapControllers();
+
+app.Run();
---
+++ codes/FirstWebAPI/FirstWebAPI/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI;
+
+
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    /// <summary>
+    /// 登录
+    /// </summary>
+    /// <param name="username"></param>
+    /// <param name="password"></param>
+    /// <returns></returns>
+    public bool Login(string username, string password)
+    {
+        // TODO:业务逻辑实现
+        Console.WriteLine(username + password);
+        return true;
+    }
+
+    [HttpPost("password")]
+    /// <summary>
+    /// 修改密码
+    /// </summary>
+    /// <param name="password"></param>
+    /// <returns></returns>
+    public bool ChangePassword(string password)
+    {
+        // TODO:业务逻辑实现
+        Console.WriteLine(password);
+        return true;
+    }
+}
