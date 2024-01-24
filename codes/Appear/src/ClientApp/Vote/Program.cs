e312b5ecdcd6f42daa5cb69ea091efb4d0c28ac7
niltor <zpty@outlook.com>
2024-01-24 17:18:32 +0800

    添加 路由处理及UserStateService

--- codes/Appear/src/ClientApp/Vote/Program.cs
+++ codes/Appear/src/ClientApp/Vote/Program.cs
@@ -1,7 +1,9 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using Vote;
+using Vote.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +12,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddFluentUIComponents();
+builder.Services.AddBlazoredLocalStorageAsSingleton();
+
+builder.Services.AddSingleton<UserStateService>();
 
 await builder.Build().RunAsync();
---
+++ codes/Appear/src/ClientApp/Vote/Services/UserStateService.cs
@@ -0,0 +1,43 @@
+﻿using Blazored.LocalStorage;
+
+namespace Vote.Services;
+
+public class UserStateService
+{
+    public Guid UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public bool IsLogin { get; set; }
+
+    private readonly ILocalStorageService _storageService;
+
+    public UserStateService(ILocalStorageService storageService)
+    {
+        _storageService = storageService;
+        GetUserStateAsync().Wait();
+    }
+
+    public void SetUserState(Guid userId, string username)
+    {
+        IsLogin = true;
+        UserId = userId;
+        UserName = username;
+
+        _storageService.SetItemAsync("UserId", UserId);
+        _storageService.SetItemAsync("UserName", UserName);
+    }
+
+    public async Task GetUserStateAsync()
+    {
+        var username = await _storageService.GetItemAsStringAsync("UserName");
+        var userId = await _storageService.GetItemAsync<Guid>("UserId");
+
+        if (userId != Guid.Empty && !string.IsNullOrWhiteSpace(username))
+        {
+            UserId = userId;
+            UserName = username;
+            IsLogin = true;
+        }
+    }
+}
