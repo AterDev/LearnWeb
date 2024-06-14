892338331e955fa144c1b3ed558c696dd6ef816a
niltor <zpty@outlook.com>
2024-06-14 16:45:52 +0800

    完善文档 3.1

--- codes/CsharpBase/Program.cs
+++ codes/CsharpBase/Program.cs
@@ -1,5 +1,28 @@
 ﻿
-int[] arr2 = new int[5];// 定义一个长度为5的整型数组
-int[] arr1 = new int[] { 1, 2, 3, 4, 5 }; // 定义一个长度为5的整型数组并初始化
-var arr = new int[] { 1, 2, 3, 4, 5 }; // 定义一个空数组，
-int[] arr3 = [1, 2, 3, 4, 5]; // 最新写法
+
+int GetMaxDivisor(int m, int n)
+{
+    if (m < n)
+    {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+    int r = m % n;
+    while (r != 0)
+    {
+        m = n;
+        n = r;
+        r = m % n;
+    }
+    return n;
+}
+
+void Output(int m, int n)
+{
+    var result = GetMaxDivisor(m, n);
+    Console.WriteLine($"{m} 和 {n} 的最大约数是: {result}");
+}
+
+Output(16, 24);
+Output(39, 13);
