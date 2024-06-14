6fb5a51121e9cf764acd98d543286d52e7e0abab
niltor <zpty@outlook.com>
2024-06-14 18:14:53 +0800

    update

--- codes/CsharpBase/Program.cs
+++ codes/CsharpBase/Program.cs
@@ -1,28 +1,3 @@
-﻿
+﻿var str = "string";
 
-int GetMaxDivisor(int m, int n)
-{
-    if (m < n)
-    {
-        int temp = m;
-        m = n;
-        n = temp;
-    }
-    int r = m % n;
-    while (r != 0)
-    {
-        m = n;
-        n = r;
-        r = m % n;
-    }
-    return n;
-}
-
-void Output(int m, int n)
-{
-    var result = GetMaxDivisor(m, n);
-    Console.WriteLine($"{m} 和 {n} 的最大约数是: {result}");
-}
-
-Output(16, 24);
-Output(39, 13);
+Console.WriteLine(str[1..^1]);
