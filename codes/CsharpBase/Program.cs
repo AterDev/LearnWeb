f0ee63cf4195bcc484c2e24066b15bb9f2d4053d
niltor <zpty@outlook.com>
2024-06-18 18:09:33 +0800

    update

--- codes/CsharpBase/Program.cs
+++ codes/CsharpBase/Program.cs
@@ -1,43 +1,33 @@
-﻿string data = """
-小明 18  2.5
-小红  19  3。2
-  张三   20    3500
-李四  21  4000 
-""";
+﻿
+using System.Text;
 
-// 使用换行符进行分割，转换成数组
-var lines = data.Split("\n");
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
 
-
-foreach (var line in lines)
+Console.WriteLine("请输入一个值，判断是整数、小数、布尔值、日期时间还是字符串");
+string? input = Console.ReadLine();// 等待输入
+if (string.IsNullOrEmpty(input))
 {
-    var parts = line.Split(" "); // 使用一个空格分隔内容
-
-    //parts = parts.Where(p => !string.IsNullOrEmpty(p)).ToArray(); // 过滤空格
-
-    string[] tempParts = new string[3];
-    var index = 0;
-    foreach (var item in parts)
-    {
-        if (!string.IsNullOrEmpty(item) && index < 3)
-        {
-            tempParts[index] = item;
-            index++;
-        }
-    }
-    parts = tempParts;
-
-    int money; // 存款(元)
-    // 对存款进行特殊兼容处理
-    string moneyStr = parts[2];
-    moneyStr = moneyStr.Replace("。", "."); // 替换全角符号
-    // 统一成元为单位的整数
-    if (moneyStr.Contains('.'))
-    {
-        double doubleMoney = double.Parse(moneyStr);// 转换成double
-        money = (int)(doubleMoney * 10000); // 转换成整数
-        parts[2] = money.ToString();
-    }
-    string newLine = string.Join(',', parts);
-    Console.WriteLine(newLine);
+    Console.WriteLine("输入为空，不判断");
+}
+else if (int.TryParse(input, out int number))
+{
+    Console.WriteLine("输入的是整数:" + number);
+}
+else if (double.TryParse(input, out double doubleNumber))
+{
+    Console.WriteLine("输入的是小数:" + doubleNumber);
+}
+else if (bool.TryParse(input, out bool boolValue))
+{
+    // true or false，不区分大小写，都会认为是bool
+    Console.WriteLine("输入的是布尔值:" + boolValue);
+}
+else if (DateTime.TryParse(input, out DateTime dateTime))
+{
+    Console.WriteLine("输入的是日期时间:" + dateTime);
+}
+else
+{
+    Console.WriteLine("输入的是字符串:" + input);
 }
