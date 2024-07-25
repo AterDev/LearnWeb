ad7f6812b6c9affda8c2912c9060fd761aacd417
niltor <zpty@outlook.com>
2024-07-25 13:53:20 +0800

    update docs

---
+++ codes/CsharpAdvance/CsharpAdvance/PrimaryConstructor.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpAdvance;
+public class PrimaryConstructor(string nameFormatter)
+{
+    public required string Name { get; set; }
+    private readonly string _nameFormatter = nameFormatter;
+
+    public PrimaryConstructor(string nameFormatter, string name) : this(nameFormatter)
+    {
+        Name = name;
+    }
+}
